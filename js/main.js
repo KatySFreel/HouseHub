560e650ca4172def3e846e086eeaadd0e9980fc7
KatySFreel <97260406+KatySFreel@users.noreply.github.com>
2023-05-30 00:32:47 +0300

    review-form

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@
 // };
 
 var mixer = mixitup(".spec__content");
-mixer.filter('.category-el');
+mixer.filter(".category-el");
 
 // burger
 
@@ -28,7 +28,7 @@ function burgerMenu(selector) {
   button.on("click", (e) => {
     e.preventDefault();
     toggleMenu();
-    
+
     lines.toggleClass("burger-menu-write");
     $("body").toggleClass("overflow");
   });
@@ -53,23 +53,103 @@ new Vivus("my-div", {
   file: "img/noh/wires.svg",
 });
 
-const line = new Vivus(
-  'line',
-  {
-    type: 'oneByOne',
-    duration: 200,
-    inViewport: true,
-    reverseStack: true
-  }
-);
-
+const line = new Vivus("line", {
+  type: "oneByOne",
+  duration: 200,
+  inViewport: true,
+  reverseStack: true,
+});
 
 AOS.init();
-
 
 let btn = document.querySelector(".offer__btn");
 
 btn.addEventListener("click", function () {
   Fancybox.show([{ src: "#dialog-content", type: "inline" }]);
-})
+});
+
+// location
+// Получаем нужный элемент
+var element = document.querySelector("#location");
+
+
+function visibleCircle(index, maxindex, elem) {
+  if (index < maxindex) {
+      elem[index].style.opacity = "1";
+      index +=1;
+      function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+      return setTimeout(() => visibleCircle(index++,maxindex,elem), getRandomArbitrary(20,70))
+  }
+
+  return 
+}
+
+locationStart = () => {
+  var svgElement = document.querySelector("#locationImg");
+  var circleElements = svgElement.querySelectorAll("circle");
+  visibleCircle(0, circleElements.length, circleElements);
+};
+
+
+var Visible = function (target) {
+  var targetPosition = {
+          top: window.pageYOffset + target.getBoundingClientRect().top,
+          left: window.pageXOffset + target.getBoundingClientRect().left,
+          right: window.pageXOffset + target.getBoundingClientRect().right,
+          bottom: window.pageYOffset + target.getBoundingClientRect().bottom,
+      },
+      windowPosition = {
+          top: window.pageYOffset,
+          left: window.pageXOffset,
+          right: window.pageXOffset + document.documentElement.clientWidth,
+          bottom: window.pageYOffset + document.documentElement.clientHeight,
+      };
+
+  if (
+      targetPosition.bottom > windowPosition.top &&
+      targetPosition.top < windowPosition.bottom &&
+      targetPosition.right > windowPosition.left &&
+      targetPosition.left < windowPosition.right
+  ) {
+      locationStart();
+  } else {
+  }
+};
+
+// Запускаем функцию при прокрутке страницы
+window.addEventListener("scroll", function () {
+  Visible(element);
+});
+
+// А также запустим функцию сразу. А то вдруг, элемент изначально видно
+Visible(element);
+
+
+Fancybox.bind('[data-fancybox]', {
+  // Custom options
+});    
+
+
+$('.reviews__menu').slick({
+  arrows: false,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
